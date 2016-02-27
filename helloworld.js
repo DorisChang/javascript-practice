4b322e6b32382fd7001bebf260b251f88a296c1e
DorisChang <doris900318@gmail.com>
2016-02-26 22:18:59 -0500

    first commit wooo

---
+++ helloworld.js
@@ -0,0 +1,57 @@
+/*
+var num = 14; //integer variable
+var decimal = 1.4 //float/double  variable
+var name = "dumb shit"; //string variable
+console.log("hello "+num);
+console.log("Have fun at math "+name);
+var x = "Diana";
+console.log ("I love you "+ x);
+*/
+/*
+//if statement
+//if
+//else if
+//else
+var age = 19;
+if (age>=18){
+	console.log("you are of age");
+}
+else if (age>12){
+	console.log("you are a teen");
+}
+else{
+	console.log("you are not of age");
+}
+*/
+/*
+var drink = "tea";
+if (drink=="wine"){
+   console.log("don't drink too much!");
+}
+else if (drink=="water"){ 
+   console.log("drink more!");
+}
+else{
+	console.log("how are you?");
+}
+*/
+/*var a = 3;
+var b = 4;
+var c = a*b;
+console.log(c)
+c = a/b;
+console.log(c);
+*/
+/*var a= 4;
+var b= 2;
+var c = a*b;
+if (a*b<20){
+	console.log("a times b is equal to "+a*b+" which is less than 20");
+}
+else if(a*b>20){
+	console.log("a times b is more than 20");
+}
+else{
+	console.log("a times b is equal to 20")
+}*/
+document.write("hello world");
